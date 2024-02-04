4b02677022b1abaced04916e341a56898e537931
haram22 <kimhr9889@handong.ac.kr>
2024-02-04 21:20:35 +0900

    [Fix] url 수정

--- Client/src/config.js
+++ Client/src/config.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = 'https://velogram.netlify.app/api/articles';
+export const API_BASE_URL = 'http://localhost:8080/api/articles';
 
 // import { API_BASE_URL } from '../config';
