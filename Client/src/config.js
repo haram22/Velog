2304f2680e21fbeed69344ef5fa3c7dab5d2b15b
haram22 <kimhr9889@handong.ac.kr>
2024-02-05 19:38:20 +0900

    url edit

--- Client/src/config.js
+++ Client/src/config.js
@@ -1,3 +1,6 @@
-export const API_BASE_URL = 'http://13.209.65.32:8080/api/articles';
+const PROXY = window.location.hostname === 'localhost' ? '' : '/proxy';
+const URL = `${PROXY}/v1/search/book.json`;
+
+export const API_BASE_URL = `http://13.209.65.32:8080/${PROXY}/api/articles`;
 
 // import { API_BASE_URL } from '../config';
