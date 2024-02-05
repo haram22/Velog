b1df342f20fdb5195890f0f03a393572392acdfa
haram22 <kimhr9889@handong.ac.kr>
2024-02-05 17:53:28 +0900

    [fix] change 배포된 url

--- Client/src/config.js
+++ Client/src/config.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = 'http://localhost:8080/api/articles';
+export const API_BASE_URL = 'http://13.209.65.32:8080/api/articles';
 
 // import { API_BASE_URL } from '../config';
